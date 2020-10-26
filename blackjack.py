260282bd458ac8fc18bb56dbe47f252cb95f7543
adonegan <48014273+adonegan@users.noreply.github.com>
2020-10-26 14:25:11 +0000

    create card class

--- blackjack.py
+++ blackjack.py
@@ -1,7 +1,24 @@
 import random
 
-# declare variables
+# Declare variables
 
 suits = ('Hearts', 'Spades', 'Diamonds', 'Clubs')
 ranks = ('Two', 'Three', 'Four', 'Five' , 'Six', 'Seven', 'Eight', 'Nine', 'Ten', 'Jack', 'Queen', 'King', 'Ace')
 values = {'Two':2, 'Three':3, 'Four':4, 'Five':5 , 'Six':6, 'Seven':7, 'Eight':8, 'Nine':9, 'Ten':10, 'Jack':10, 'Queen':10, 'King':10, 'Ace':11}
+
+# Declare game state
+
+game_on = False
+
+# Create card class
+
+class Card:
+
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    
+    def __str__(self):
+        return self.rank + ' of ' + self.suit
+
+
